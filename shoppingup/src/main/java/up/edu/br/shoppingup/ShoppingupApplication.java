b9d53cd833fbf0ad6126122912e1ef21c3e8915e
Joao Justo <joao.justo@roit.com.br>
2020-05-02 15:46:31 -0300

    first commit

---
+++ shoppingup/src/main/java/up/edu/br/shoppingup/ShoppingupApplication.java
@@ -0,0 +1,13 @@
+package up.edu.br.shoppingup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingupApplication.class, args);
+	}
+
+}
---
+++ shoppingup/src/test/java/up/edu/br/shoppingup/ShoppingupApplicationTests.java
@@ -0,0 +1,13 @@
+package up.edu.br.shoppingup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
